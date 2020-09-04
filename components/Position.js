7a92f2c9cc26c7553dcd177854b7d40467a9b58f
SmolyankaA <56487177+HannaBusko@users.noreply.github.com>
2020-09-04 16:30:54 +0300

    feat: finish 1tab

--- components/Position.js
+++ components/Position.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 
 import Geolocation from "@react-native-community/geolocation";
 import Geocoder from "react-native-geocoding";
 
-Geocoder.init("AIzaSyAnYsY86_y6XONni38aVCoKty8ehA7icF4"); 
+Geocoder.init("AIzaSyAnYsY86_y6XONni38aVCoKty8ehA7icF4");
 
 class CurrentPosition extends Component {
   constructor(props) {
@@ -13,7 +13,8 @@ class CurrentPosition extends Component {
       latitude: 0,
       longitude: 0,
       error: null,
-      address: null
+      address: null,
+      forecast: null
     };;
   }
 
@@ -24,18 +25,26 @@ class CurrentPosition extends Component {
           latitude: position.coords.latitude,
           longitude: position.coords.longitude
         });
-        
+
         Geocoder.from(position.coords.latitude, position.coords.longitude).then(json => {
           console.log(json);
-
-          var addressComponent = json.results[0].address_components;
-
           this.setState({
-            address: addressComponent
+            address: json.results[0].formatted_address
           });
-
-          console.log(addressComponent);
         })
+        let url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&appid=4bb26d063345b0b5f4a3ae9fa9e5c78c' + '&units=metric&lang=ru';
+        fetch(url)
+          .then(response => {
+            return response.json()
+          })
+          .then(data => {
+            console.log(data);
+            let weather = data.weather[0].description[0].toUpperCase() + data.weather[0].description.substring(1);
+            this.setState(
+              {
+                forecast: weather + ". Температура воздуха: "+ data.main.temp + " C." 
+              });
+          })
       }
     }, (error) => {
       this.setState({ error: error.message }),
@@ -53,11 +62,14 @@ class CurrentPosition extends Component {
       >
         {this.state.error ? < Text > Error : {
           this.state.error} </Text> : null}
-        <Text>Latitude: {this.state.latitude}</Text>
-        <Text>Longitude: {this.state.longitude}</Text>
+        <Text>Широта: {this.state.latitude}</Text>
+        <Text>Долгота: {this.state.longitude}</Text>
+        <Text>Адрес: {this.state.address}</Text>
+        <Text>Погода: {this.state.forecast}</Text>    
+      
       </View>
     );
   }
 }
 
-export default CurrentPosition;
+export default CurrentPosition;  
